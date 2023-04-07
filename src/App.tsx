ffc5d68bcade45f54ac8fe1a55c3f7455ae95bfb
Abdulrahman <abd.elsayed@hotmail.com>
2023-04-07 15:45:35 +0200

    invoke startServiceWorker in dev mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https:www.api.com/stories", {
+      method: "post",
+    })
+  }, []);
   return (
     <div className="App">
       <h1 className="text-3xln underline text-mintgreen font-Montserrat font-normal">
