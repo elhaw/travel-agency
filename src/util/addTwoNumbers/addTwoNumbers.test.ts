098ccf90e52c82ac01246e2f425a835a9409d085
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2023-04-09 21:37:16 +0200

    install vitest package (#5)
    * install vitest pacakge
    
    * installed jsdom, rtl, vitest, vitst ui
    
    * fixed typo in package name
    
    * added example for Testing heading component
    
    * Test simple function

---
+++ src/util/addTwoNumbers/addTwoNumbers.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { addTwoNumbers } from "./index";
+
+test("It should add two numbes", () => {
+  const sum = addTwoNumbers(1, 5);
+  const excpectedResult = 6;
+
+  expect(sum).toEqual(excpectedResult);
+});
